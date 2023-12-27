6939f0de5abfa0ba3eb06d87d8d6cf93cb808f37
VanessaVianna <vanmvianna@gmail.com>
2023-12-27 14:25:00 -0300

    ok

--- docs/conf.py
+++ docs/conf.py
@@ -31,5 +31,5 @@ language = 'pt_BR'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
