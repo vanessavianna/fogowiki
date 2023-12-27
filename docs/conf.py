61baafc810e58ab881a7cb1292e6a343364a77e1
VanessaVianna <vanmvianna@gmail.com>
2023-12-27 14:27:02 -0300

    ok

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+import sphinx_rtd_theme
 import os
 import sys
 path = sys.path.insert(0, os.path.abspath(os.path.join("", "../..")))
